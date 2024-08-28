ac435231a1037c495cb3d31045b550280f096615
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-08-28 18:14:04 -0400

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; } = string.Empty;
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; } = string.Empty;
+    }
+}
