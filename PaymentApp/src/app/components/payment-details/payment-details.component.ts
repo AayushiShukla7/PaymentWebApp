2d98c948b0ff3b44d3e66d79fbaa0dd8106da15e
Aayushi Shukla <53366218+AayushiShukla7@users.noreply.github.com>
2024-08-28 18:44:11 -0400

    More setup

---
+++ PaymentApp/src/app/components/payment-details/payment-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-details.component.html',
+  styles: ``
+})
+export class PaymentDetailsComponent {
+
+}
